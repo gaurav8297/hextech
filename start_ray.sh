130899c08d60b5ce0363ce3a12064f857cf8c652
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-11-17 01:09:14 -0500

    add ray script

--- start_ray.sh
+++ start_ray.sh
@@ -5,7 +5,7 @@ NODE_TYPE="$2"
 
 RAY_START_CMD="ray start --block"
 if [ "${NODE_TYPE}" == "--head" ]; then
-    RAY_START_CMD+=" --head --port=6379"t
+    RAY_START_CMD+=" --head --port=6379"
 else
     RAY_START_CMD+=" --address=${HEAD_NODE_ADDRESS}:6379"
 fi
